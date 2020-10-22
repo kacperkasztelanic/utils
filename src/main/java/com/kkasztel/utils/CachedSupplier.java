19024cd9c4f64d209a49545d1a0e4f6204328d45
Kacper Kasztelanic <kcpr51@hotmail.com>
2020-10-22 20:15:12 +0200

    Add CachedSupplier

---
+++ src/main/java/com/kkasztel/utils/CachedSupplier.java
@@ -0,0 +1,34 @@
+package com.kkasztel.utils;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(staticName = "of")
+public class CachedSupplier<T> implements Supplier<T> {
+
+    private final Supplier<T> delegate;
+    private final AtomicReference<T> value = new AtomicReference<>();
+
+    @Override
+    public T get() {
+        T val = value.get();
+        if (val == null) {
+            synchronized (value) {
+                val = value.get();
+                if (val == null) {
+                    val = delegate.get();
+                    value.set(val);
+                }
+            }
+        }
+        return value.get();
+    }
+
+    public void invalidate() {
+        synchronized (value) {
+            value.set(null);
+        }
+    }
+}
