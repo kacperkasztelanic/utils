e6dcd3252641ca2234f7d0301fa296f68f9ae35a
Kacper Kasztelanic <kcpr51@hotmail.com>
2022-03-30 23:19:08 +0200

    Add Pair

---
+++ src/main/java/com/kkasztel/utils/tuple/Pair.java
@@ -0,0 +1,38 @@
+package com.kkasztel.utils.tuple;
+
+import lombok.ToString;
+import lombok.Value;
+
+import java.util.Map;
+
+@Value(staticConstructor = "of")
+@ToString(includeFieldNames = false)
+public class Pair<L, R> {
+
+    L left;
+    R right;
+
+    public static <L, R> Pair<L, R> of(Pair<? extends L, ? extends R> pair) {
+        return of(pair.getLeft(), pair.getRight());
+    }
+
+    public static <L, R> Pair<L, R> of(Map.Entry<? extends L, ? extends R> entry) {
+        return of(entry.getKey(), entry.getValue());
+    }
+
+    public L getFirst() {
+        return left;
+    }
+
+    public R getSecond() {
+        return right;
+    }
+
+    public L getKey() {
+        return left;
+    }
+
+    public R getValue() {
+        return right;
+    }
+}
